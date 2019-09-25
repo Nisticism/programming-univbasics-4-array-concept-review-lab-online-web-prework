4c6eee56c04a52801816be9cdfdfd54e8e956a68
Hans Foster <fosterhans@gmail.com>
2019-09-25 23:33:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,9 @@ def find_element_index(array, value_to_find)
   }
 
 def find_max_value(array)
-  largest_number = 0
+  largest_number = array[0]
+  array_length = array.length
+  array_length.times |l
 end
 
 def find_min_value(array)
